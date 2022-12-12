62f1537ec19becc6bc5ec7740ed5c5cad6336a0e
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-12-12 13:37:07 +0100

    balazs.matyassy: bugfix (3.0 -> 2.0)

--- src/bolyai/Main.java
+++ src/bolyai/Main.java
@@ -3,7 +3,8 @@ package bolyai;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("3.0");
+        // BUGFIX
+        System.out.println("2.0");
     }
 
 }
