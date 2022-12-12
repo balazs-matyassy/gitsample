40309b5a1062ce89f4ab69de8ee0c10458dffaea
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-12-12 13:34:12 +0100

    balazs.matyassy: hello world

--- src/bolyai/Main.java
+++ src/bolyai/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("3.0");
+        // új feature
+        System.out.println("Hello World!!!");
     }
 
 }
