24e30b35aa8cc97a1f6141ded71287bc22c81637
Vidra Gergő <vidragery@gmail.com>
2022-12-12 15:30:27 +0100

    gery: asd

--- src/bolyai/Main.java
+++ src/bolyai/Main.java
@@ -3,9 +3,9 @@ package bolyai;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("2.0");
+        System.out.println("4.0");
         // új feature
-        System.out.println("Hello World!!!");
+        System.out.println("Hello World!!!!");
     }
 
 }
