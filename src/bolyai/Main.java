84af33928dcc4401b7c9b5c3fd0a77147f482c52
Vidra Gergő <vidragery@gmail.com>
2022-12-12 15:37:07 +0100

    gery: newest update2

--- src/bolyai/Main.java
+++ src/bolyai/Main.java
@@ -3,7 +3,7 @@ package bolyai;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("4.0");
+        System.out.println("5.0");
         // új feature
         System.out.println("Hello World!!!!");
     }
